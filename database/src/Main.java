a0380461632b47f04eeda55048486424f90408d7
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-02-25 12:03:55 +0300

    Add files via upload

---
+++ database/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String s1 = "CSE12124551";
+        System.out.println(s1.length());
+        System.out.println(s1.substring(s1.length()/2));
+    }
+}
