dccf062e2cf2e20a3348c4668aa3b6f231e4bb58
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-02-25 12:01:58 +0300

    Add files via upload

---
+++ 07.ders/src/yapiciMetotlar.java
@@ -0,0 +1,10 @@
+
+public class yapiciMetotlar {
+
+}
+
+public class yapiciMetotlar{
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ 07.ders/src/ögrenci.java
@@ -0,0 +1,35 @@
+
+public class ögrenci {
+	private String ad;
+	private String soyad;
+	private int okulNo;
+	private int dogumYili;
+	
+	public String getAd() {
+		return ad;
+	}
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+	public String getSoyad() {
+		return soyad;
+	}
+	public void setSoyad(String soyad) {
+		this.soyad = soyad;
+	}
+	public int getOkulNo() {
+		return okulNo;
+	}
+	public void setOkulNo(int okulNo) {
+		this.okulNo = okulNo;
+	}
+	public int getDogumYili() {
+		return dogumYili;
+	}
+	public void setDogumYili(int dogumYili) {
+		this.dogumYili = dogumYili;
+	}
+	
+	
+
+}
