8820848764b32715a022d9d132231ffdb64c947b
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-02-25 12:06:38 +0300

    Add files via upload

---
+++ Extends1/src/kalitim.java
@@ -0,0 +1,21 @@
+
+class kalitim {
+	//int maxSpeed = 120;
+	public void vroom() {
+		System.out.println("vromm");
+	}
+
+}
+class Car extends kalitim{
+	/*
+	int maxSpeed = 100;
+	
+	public void display() {
+		System.out.println(super.maxSpeed);
+	}
+	*/
+	
+}
+
+
+
