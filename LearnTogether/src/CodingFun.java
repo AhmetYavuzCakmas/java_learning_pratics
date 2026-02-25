1d99f9e4d09977f7fee3f1c1084ede886a2709fa
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-02-25 12:10:41 +0300

    Add files via upload

---
+++ LearnTogether/src/CodingFun.java
@@ -0,0 +1,15 @@
+
+public class CodingFun {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student(1,"bra");
+		Student s2 = new Student(2,"jackie");
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
+		
+	}
+
+}
